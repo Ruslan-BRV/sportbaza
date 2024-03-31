c50c78c13f8bbb05b4c9cb54a0261d21d4004bc4
Ruslan_BRV <ruslan10897@mail.ru>
2024-03-31 19:29:46 +0300

    Пробная версия лайк, дизлайк

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from news.models import News
-
+from django.db.models import F
 # Create your views here.
 
 def newsPage(request):
@@ -12,3 +12,33 @@ def newsPage(request):
 
     return render(request, "news/news.html", context=context)
 
+def addLike(request, news_id):
+    
+    news = get_object_or_404(News, pk=news_id)
+
+    news.like = F('like') + 1
+    news.save()
+    
+    news.refresh_from_db()
+    news = News.objects.all()
+    
+    context = {
+        "news": news
+    }
+
+    return render(request, "news/news.html", context)  
+
+def addDizlike(request, news_id):
+    news = get_object_or_404(News, pk=news_id)
+
+    news.dizlike = F('dizlike') + 1
+    news.save()
+    
+    news.refresh_from_db()
+    news = News.objects.all()
+    
+    context = {
+        "news": news
+    }
+
+    return render(request, "news/news.html", context)  
