f379196cd78d7dbe8471fb265627ce59d001e0fc
Ruslan_BRV <ruslan10897@mail.ru>
2024-04-17 14:11:56 +0300

    Поменял цвет оформления

--- sportbaza/settings.py
+++ sportbaza/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'debug_toolbar',
     'calculator',
     'programms',
     'news'
@@ -62,7 +63,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = [
+        '127.0.0.1',
+    ]
 
 ROOT_URLCONF = 'sportbaza.urls'
 
--- sportbaza/urls.py
+++ sportbaza/urls.py
@@ -16,3 +16,10 @@ urlpatterns = [
     path("calculator/", include("calculator.urls")),
     path("registration/", regUser),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+      import debug_toolbar
+      urlpatterns = [
+          path('__debug__/', include(debug_toolbar.urls)),
+          # другие URL-паттерны
+      ] + urlpatterns
